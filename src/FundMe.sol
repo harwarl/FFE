b19533336b00974a84d1061c0da5a051297ab773
harwarl <harwarl87@gmail.com>
2025-01-05 00:02:54 +0100

    current

---
+++ src/FundMe.sol
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.18;
+
+// import {AggregatorV3Interface} from "@chainlink/contracts/src/v0.8/shared/interfaces/AggregatorV3Interface.sol";
+
+error NotOwner();
+
+contract FundMe {
+    constructor(){
+
+    }
+}
